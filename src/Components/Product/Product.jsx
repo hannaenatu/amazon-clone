e0fef0936bf17c5526f7c29eecfbbe8412925aa1
hannaenatu <hannaenatu@yahoo.com>
2024-09-27 18:04:57 -0400

    update product css name

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,38 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import ProductCard from './ProductCard'
-import classes from './product.module.css'
-import Loader from '../Loader/Loader'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductCard from "./ProductCard";
+import classes from "./Product.module.css";
+import Loader from "../Loader/Loader";
 
 function Product() {
-    const [products, setProducts] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-    axios.get('https://fakestoreapi.com/products')
-    .then((res) => {
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
         // console.log(res);
-        setProducts(res.data)
-        setIsLoading(false)
-    }).catch((err) => {
+        setProducts(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
         console.log(err);
         setIsLoading(false);
-    });
-    }, []);
+      });
+  }, []);
   return (
     <>
-    {
-    isLoading?(<Loader/>) : (<section className={classes.products_container}>
-    {
-        products?.map((singleProduct) => { 
-         return <ProductCard  renderAdd={true} product={singleProduct} key={singleProduct.id} />
-})
-    }
-
-    </section>)
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className={classes.products_container}>
+          {products?.map((singleProduct) => {
+            return (
+              <ProductCard
+                renderAdd={true}
+                product={singleProduct}
+                key={singleProduct.id}
+              />
+            );
+          })}
+        </section>
+      )}
+    </>
+  );
 }
-  </>
-  )
-};
 
 export default Product;
